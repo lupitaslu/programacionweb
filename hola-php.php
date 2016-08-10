782953e1a960ddb9e87f2e159aa2237733164933
lupitaslu <lulujplupita@hotmail.com>
2016-08-09 19:03:57 -0500

    agregamos presentacion y comportamiento

---
+++ hola-php.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="es">
+   <head>
+         <title> hola php</title>
+		     <style type="text/css"> /*presentacion*/
+			    h1{color : peru;}
+				p{color : #abcdf7;}
+				li:first-letter{font-size:2em;}
+			
+			 </style>
+			 <!-- Latest compiled and minified CSS -->
+             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+             <!-- Optional theme -->
+             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+             <!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	 </head>
+	 <body>
+	    <script type="text/javascript">	
+     	 alert ("hola:"+ prompt("Ingresa tu nombre"));
+		 </script>
+	    <h1> hola php</h1>
+		<p>dias de la semana</p>
+		<a class="btn btn-success" href="http://www.google.com"> Ir a google </a>
+		<ol>
+		    <li> lunes</li>
+			<li> martes</li>
+			<li> miercoles</li>
+			<li> jueves</li>
+			<li> viernes</li>
+			<li> sabado</li>
+			<li> domingo</li>
+		</ol>
+            		
+	</body>
+</html>	
